1012648435caca8e013ba7736209347eefe80d42
Jerome <zjrtomjohn@hotmail.com>
2017-11-02 22:51:58 +0800

    Fix Q6 submit

--- q6_submit.php
+++ q6_submit.php
@@ -5,7 +5,6 @@ $username="jrzhou";
 $password="1293Zjr";
 $database="jrzhou";
 
-
 // Step 1. Connect to database server
 mysql_connect($host,$username,$password);
 
@@ -25,14 +24,16 @@ echo "</head>";
 echo "<body align=center>";
 
 // Question content
-echo "<p align='center'><b>Q6: </b> isplay a drop-down menu which contains the teamID and team name of all the teams.</p><br><br>";
-echo "<form action='q6.php' method='GET'>"
-echo "<select name='teamID'>"
+echo "<p align='center'><b>Q6: </b>Display a drop-down menu which c;ontains the teamID and team name of all the teams.</p><br><br>";
+echo "<form action='q6.php' method='GET'>";
+echo "Team ID & Team Name: ";
+echo "<select name='teamID'>";
 
 while($row = mysql_fetch_array($result, MYSQL_ASSOC))
 {
 	echo "<option value=".$row['teamID'].">".$row['teamID'].": ".$row['name']."</option>";
 }
+echo "</select>";
 
 echo "<input type='submit' value='submit'>";
 echo "</form>";
